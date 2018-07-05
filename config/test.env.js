514e3b27fcfdbe19efa8e95954afca5e1a97005b
“chenjiaguang” <598423644@qq.com>
2018-07-05 10:12:53 +0800

    添加make脚本      chenjiaguang

---
+++ config/test.env.js
@@ -0,0 +1,9 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"test"',
+  API_DOMAIN: '"http://fanttest.fantuanlife.com"',
+  LOCATION_DOMAIN: '"https://fanttest.fantuanlife.com/cweb/index.html#"'
+})
