18f2a621158e4f8cd7827afb2e0a7bd04fa19f35
chenjiayao <598423644@qq.com>
2018-08-31 14:32:46 +0800

    列表显示小图   chenjiaguang

--- src/components/ImageContainer.vue
+++ src/components/ImageContainer.vue
@@ -2,13 +2,13 @@
   <div style="width: 100%;" class="clearfix">
     <template v-for="(item, idx) in images">
       <div :key="item.sign || item.id || idx" v-if="isAndroid()">
-        <div v-if="isUpload" :key="item.sign || item.id || idx" @click.stop="previewImage(idx)" :style="{marginTop: idx < 3 ? 0 : '1.055%', backgroundImage: 'url(' + (item.localUrl || item.url) + ')'}" class="item-container two-and-more fl" :class="{left: idx % 3 === 0}">
+        <div v-if="isUpload" :key="item.sign || item.id || idx" @click.stop="previewImage(idx)" :style="{marginTop: idx < 3 ? 0 : '1.055%', backgroundImage: 'url(' + (item.localUrl ||  item.url) + ')'}" class="item-container two-and-more fl" :class="{left: idx % 3 === 0}">
           <!-- <img :src="item.localUrl || item.url" class="image-item" :class="{horizontal: images.length === 1 && (Number(item.width) / Number(item.height) >= 1.44) || images.length > 1 && Number(item.width) >= Number(item.height), vertical: images.length === 1 && (Number(item.width) / Number(item.height) < 1.44) || images.length > 1 && Number(item.width) < Number(item.height)}" /> -->
           <div class="long-tag" v-if="Number(item.height) / Number(item.width) > 4">长图</div>
           <div class="delete-btn iconfont icon-guanbi" v-if="showDelete" @click.stop="deleteImage(item, idx)"></div>
           <div class="status-box" v-if="item.status === 'submitting' || item.status === 'error' || item.status === 'reading'"><span class="status-text">{{statusText[item.status]}}</span></div>
         </div>
-        <div v-else v-lazy:background-image="item.url" :key="item.sign || item.id || idx" @click.stop="previewImage(idx)" :style="{marginTop: idx < 3 ? 0 : '1.055%'}" class="item-container fl" :class="{one: images.length === 1 && !isUpload, 'two-and-more': images.length > 1 || isUpload, left: idx % 3 === 0, horizontal: Number(item.width) >= Number(item.height), vertical: Number(item.width) < Number(item.height)}">
+        <div v-else v-lazy:background-image="item.compress || item.url" :key="item.sign || item.id || idx" @click.stop="previewImage(idx)" :style="{marginTop: idx < 3 ? 0 : '1.055%'}" class="item-container fl" :class="{one: images.length === 1 && !isUpload, 'two-and-more': images.length > 1 || isUpload, left: idx % 3 === 0, horizontal: Number(item.width) >= Number(item.height), vertical: Number(item.width) < Number(item.height)}">
           <!-- <img :data-src="item.localUrl || item.url" class="image-item" :class="{horizontal: images.length === 1 && (Number(item.width) / Number(item.height) >= 1.44) || images.length > 1 && Number(item.width) >= Number(item.height), vertical: images.length === 1 && (Number(item.width) / Number(item.height) < 1.44) || images.length > 1 && Number(item.width) < Number(item.height)}" /> -->
           <div class="long-tag" v-if="Number(item.height) / Number(item.width) > 4">长图</div>
           <div class="delete-btn iconfont icon-guanbi" v-if="showDelete" @click.stop="deleteImage(item, idx)"></div>
@@ -22,7 +22,7 @@
           <div class="delete-btn iconfont icon-guanbi" v-if="showDelete" @click.stop="deleteImage(item, idx)"></div>
           <div class="status-box" v-if="item.status === 'submitting' || item.status === 'error' || item.status === 'reading'"><span class="status-text">{{statusText[item.status]}}</span></div>
         </div>
-        <div v-else v-lazy:background-image="item.url" :key="item.sign || item.id || idx" @click.stop="previewImage(idx)" :style="{marginTop: idx < 3 ? 0 : '1.055%'}" class="item-container fl" :class="{one: images.length === 1 && !isUpload, 'two-and-more': images.length > 1 || isUpload, left: idx % 3 === 0, horizontal: Number(item.width) >= Number(item.height), vertical: Number(item.width) < Number(item.height)}">
+        <div v-else v-lazy:background-image="item.compress || item.url" :key="item.sign || item.id || idx" @click.stop="previewImage(idx)" :style="{marginTop: idx < 3 ? 0 : '1.055%'}" class="item-container fl" :class="{one: images.length === 1 && !isUpload, 'two-and-more': images.length > 1 || isUpload, left: idx % 3 === 0, horizontal: Number(item.width) >= Number(item.height), vertical: Number(item.width) < Number(item.height)}">
           <!-- <img :data-src="item.localUrl || item.url" class="image-item" :class="{horizontal: images.length === 1 && (Number(item.width) / Number(item.height) >= 1.44) || images.length > 1 && Number(item.width) >= Number(item.height), vertical: images.length === 1 && (Number(item.width) / Number(item.height) < 1.44) || images.length > 1 && Number(item.width) < Number(item.height)}" /> -->
           <div class="long-tag" v-if="Number(item.height) / Number(item.width) > 4">长图</div>
           <div class="delete-btn iconfont icon-guanbi" v-if="showDelete" @click.stop="deleteImage(item, idx)"></div>
