e158c4077a5d23a9a68f3eec4e22e544ff4b7d9d
chenjiayao <598423644@qq.com>
2018-09-12 17:12:02 +0800

    图片更改、视频播放测试     chenjiaguang

---
+++ src/pages/testVideo.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="video-page">
+    <div class="">播放视频</div>
+    <video id="fantuan_video" class="my-video video-js vjs-default-skin" controls preload="none" poster="http://vjs.zencdn.net/v/oceans.png">
+      <source src="https://mat1.gtimg.com/v/dee/Detective.mp4" type="video/mp4">
+    </video>
+  </div>
+</template>
+
+<script>
+import videojs from 'video.js'
+import 'video.js/dist/video-js.min.css'
+export default {
+  data () {
+    return {
+      video: null
+    }
+  },
+  mounted () {
+    this.video = videojs('fantuan_video', {
+      muted: true,
+      controls: true,
+      loop: false,
+      width: window.innerWidth,
+      height: window.innerHeight
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .video-page{
+    width: 80%;
+  }
+  .my-video{
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+  }
+</style>
