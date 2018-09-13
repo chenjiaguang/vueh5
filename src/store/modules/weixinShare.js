9c7c5329293f7c1235f59d51adddc81605d26969
hyf <459909293@qq.com>
2018-09-13 10:53:46 +0800

    hyf qq分享

--- src/store/modules/weixinShare.js
+++ src/store/modules/weixinShare.js
@@ -37,6 +37,21 @@ export default {
             }
           }
         })
+        global.wx.onMenuShareQQ({
+          title: payload.title, // 分享标题
+          desc: payload.desc, // 分享描述
+          link: payload.url, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: payload.imgUrl, // 分享图标
+          success: function () {
+            console.log('type', state.type)
+            if (state.type) {
+              ajax('/jv/share/article/addPoint', { data: { type: state.type } })
+            }
+          },
+          cancel: function () {
+          // 用户取消分享后执行的回调函数
+          }
+        })
       })
     }
   }
