ca51996af1745e81b1f5127a26a56617173ee10c
hyf <459909293@qq.com>
2018-11-16 11:29:17 +0800

    hyf cors

---
+++ src/pages/CORS.vue
@@ -0,0 +1,34 @@
+<template>
+  <iframe class="iframe" height="1000" frameborder="0" src="about:blank" scrolling="no" id="content"></iframe>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  mounted () {
+    this.fetch()
+  },
+  methods: {
+    fetch () {
+      let url = this.$route.query && this.$route.query.url
+      this.$ajax('/jv/anonymous/call/get?url=' + url, {
+        method: 'GET'
+      }).then(res => {
+        let text = res.data.result
+        this.setContent(text)
+      })
+    },
+    setContent (data) {
+      let iframe = document.getElementById('content')
+      iframe.contentDocument.body.innerHTML = data
+    }
+  }
+}
+</script>
+
+<style scoped>
+.iframe {
+}
+</style>
